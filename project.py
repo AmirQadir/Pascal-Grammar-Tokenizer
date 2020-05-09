f175043b255f502b2daf0bd36eaa73439aa551e1
AmirQadir <K163641@nu.edu.pk>
2020-05-10 00:55:29 +0500

    Initialization of Project

---
+++ project.py
@@ -0,0 +1,18 @@
+
+
+def main():
+
+    with open("etc.txt", "r",encoding='utf-8') as fileReader:
+        for line in fileReader:
+            line = line.strip()
+            if line:
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
