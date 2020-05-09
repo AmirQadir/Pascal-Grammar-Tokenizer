fccfc12d11b1e978d2373d5779e9d7a6ec3fc40e
AmirQadir <K163641@nu.edu.pk>
2020-05-10 01:12:43 +0500

    Introduced keywords and symbol sets

--- project.py
+++ project.py
@@ -1,12 +1,13 @@
-
-
 def main():
+    keywords = {"program", "real", "integer", "if", "then", "else", "while", "repeat", "readln", "write", "writeln",
+                "or", "div", "mod",
+                "and", "true", "false", "not", "trunc", "do", "until","end", "begin"}
+    sym = {";", ".", ":", ":=", "+", "-", "*", "/", "(", ")", "<", ">", "<=", ">=", "<>", "="}
 
-    with open("etc.txt", "r",encoding='utf-8') as fileReader:
+    with open("etc.txt", "r", encoding='utf-8') as fileReader:
         for line in fileReader:
             line = line.strip()
             if line:
-
 
 
 
