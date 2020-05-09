d049adae6d6d53fd11904b5c8a66065682498b3d
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-05-10 01:56:11 +0500

    Added logic for recognition of keywords, identifiers and symbols (basic)

--- project.py
+++ project.py
@@ -1,18 +1,59 @@
-def main():
-    keywords = {"program", "real", "integer", "if", "then", "else", "while", "repeat", "readln", "write", "writeln",
+keywords = {"program", "var", "real", "integer", "if", "then", "else", "while", "repeat", "readln", "write", "writeln",
                 "or", "div", "mod",
                 "and", "true", "false", "not", "trunc", "do", "until","end", "begin"}
-    sym = {";", ".", ":", ":=", "+", "-", "*", "/", "(", ")", "<", ">", "<=", ">=", "<>", "="}
+sym = {";", ".", ":", ":=", "+", "-", "*", "/", "(", ")", "<", ">", "<=", ">=", "<>", "="}
 
+def status(buildup):
+    if buildup.strip():
+        if buildup in keywords:
+            print("KEYWORD:", buildup)
+        elif buildup in sym:
+            print("SYM:", buildup)
+        else:
+            print("ID:", buildup)
+            # ids.append(buildup)
+
+def main():
     with open("etc.txt", "r", encoding='utf-8') as fileReader:
+        ids_by_line = []
         for line in fileReader:
             line = line.strip()
-            if line:
+            if not line:
+                continue
+            
+            buildup = ""
 
+            # ids = []
 
+            looking_for_more_sym = False
 
+            # TODO: Specialized logic for stringConst
+            for char in line:
+                if char not in sym and char != " ":
+                    if looking_for_more_sym:
+                        looking_for_more_sym = False
+                        status(buildup)
+                        buildup = char
+                    else:
+                        buildup += char
+                elif char in sym:
+                    if looking_for_more_sym:
+                        looking_for_more_sym = False
+                        buildup += char
+                        status(buildup)
+                        buildup = ""
+                    else:
+                        status(buildup)
+                        looking_for_more_sym = True
+                        buildup = char
+                else:
+                    status(buildup)
+                    buildup = ""
+            
+            status(buildup)
+            buildup = ""
 
-
+            # ids_by_line.append(id)
 
 
 if __name__ == "__main__":
